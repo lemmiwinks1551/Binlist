c13b10f8cceb53170f4bb6749921bb3c47eb7535
Kirill <ananevka@mail.ru>
2025-06-24 21:38:18 +0300

    v.1

---
+++ app/src/main/java/com/example/binlist/presentation/ui/navigation/Common.kt
@@ -0,0 +1,24 @@
+package com.example.binlist.presentation.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+
+fun getNavigationItems(): List<BottomNavItem> {
+    return listOf(
+        BottomNavItem(
+            title = "Главная",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            route = Routes.Home
+        ),
+        BottomNavItem(
+            title = "Настройки",
+            selectedIcon = Icons.Filled.Settings,
+            unselectedIcon = Icons.Outlined.Settings,
+            route = Routes.Settings
+        )
+    )
+}
