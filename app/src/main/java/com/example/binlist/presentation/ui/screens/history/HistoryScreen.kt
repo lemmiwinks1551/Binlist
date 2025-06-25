583a072aae675ef03f4d3b2257ef65b3c1c65551
Kirill <ananevka@mail.ru>
2025-06-25 16:18:08 +0300

    + box

--- app/src/main/java/com/example/binlist/presentation/ui/screens/history/HistoryScreen.kt
+++ app/src/main/java/com/example/binlist/presentation/ui/screens/history/HistoryScreen.kt
@@ -1,13 +1,18 @@
 package com.example.binlist.presentation.ui.screens.history
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -16,10 +21,25 @@ import com.example.binlist.domain.model.BinInfoCard
 @Composable
 fun HistoryScreen(viewModel: HistoryViewModel = hiltViewModel()) {
     val state by viewModel.state.collectAsState()
-    LazyColumn(modifier = Modifier.padding(16.dp)) {
-        items(state.history) { item ->
-            BinInfoCard(info = item)
-            Spacer(modifier = Modifier.height(8.dp))
+
+    if (state.history.isEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Здесь пока ничего нет", style = MaterialTheme.typography.bodyMedium)
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(state.history) { item ->
+                BinInfoCard(info = item)
+                Spacer(modifier = Modifier.height(8.dp))
+            }
         }
     }
 }
+
