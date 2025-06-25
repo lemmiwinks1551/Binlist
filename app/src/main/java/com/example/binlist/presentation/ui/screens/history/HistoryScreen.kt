241adf255bdf49780386dea3a64e01da8c72e193
Kirill <ananevka@mail.ru>
2025-06-25 15:57:03 +0300

    History screen + view model

--- app/src/main/java/com/example/binlist/presentation/ui/screens/history/HistoryScreen.kt
+++ app/src/main/java/com/example/binlist/presentation/ui/screens/history/HistoryScreen.kt
@@ -1,9 +1,25 @@
 package com.example.binlist.presentation.ui.screens.history
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.binlist.domain.model.BinInfoCard
 
 @Composable
-fun HistoryScreen() {
-
+fun HistoryScreen(viewModel: HistoryViewModel = hiltViewModel()) {
+    val state by viewModel.state.collectAsState()
+    LazyColumn(modifier = Modifier.padding(16.dp)) {
+        items(state.history) { item ->
+            BinInfoCard(info = item)
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
 }
-
--- app/src/main/java/com/example/binlist/presentation/ui/screens/history/HistoryViewModel.kt
+++ app/src/main/java/com/example/binlist/presentation/ui/screens/history/HistoryViewModel.kt
@@ -1,10 +1,31 @@
 package com.example.binlist.presentation.ui.screens.history
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.binlist.domain.model.HistoryMviState
+import com.example.binlist.domain.repository.BinInfoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HistoryViewModel @Inject constructor() : ViewModel() {
+class HistoryViewModel @Inject constructor(
+    private val repo: BinInfoRepository
+) : ViewModel() {
 
+    private val _state = MutableStateFlow(HistoryMviState())
+    val state: StateFlow<HistoryMviState> = _state
+
+    init {
+        viewModelScope.launch {
+            repo.getHistory().collect { list ->
+                _state.update { it.copy(history = list) }
+            }
+        }
+    }
 }
+
