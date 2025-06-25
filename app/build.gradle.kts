8499fad2871798e1812041d2d1a7835dc0809ddd
Kirill <ananevka@mail.ru>
2025-06-25 14:53:18 +0300

    + Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,11 @@ dependencies {
 
     // LeakCanary
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.14")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 }
 
 kapt {
