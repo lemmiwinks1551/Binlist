bc5001c1e35862d1f0d1e62946c66d3756616547
Kirill <ananevka@mail.ru>
2025-06-25 15:17:17 +0300

    + Box for centring content

--- app/src/main/java/com/example/binlist/presentation/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/binlist/presentation/ui/screens/home/HomeScreen.kt
@@ -1,11 +1,74 @@
 package com.example.binlist.presentation.ui.screens.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.binlist.domain.model.BinInfoCard
 
 @Composable
-@Preview(showBackground = true)
-fun HomeScreen(
-) {
-}
+@Preview
+fun HomeScreen(viewModel: HomeScreenViewModel = hiltViewModel()) {
+    val state by viewModel.state.collectAsState()
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        OutlinedTextField(
+            value = state.inputBin,
+            onValueChange = { viewModel.onBinChanged(it) },
+            label = { Text("Введите BIN") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { viewModel.onSearchClicked() },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = !state.isLoading
+        ) {
+            Text("Поиск")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            when {
+                state.isLoading -> {
+                    CircularProgressIndicator()
+                }
+
+                state.errorMessage != null -> {
+                    Text(text = state.errorMessage!!, color = Color.Red)
+                }
+
+                state.binInfo != null -> {
+                    BinInfoCard(info = state.binInfo!!)
+                }
+            }
+        }
+    }
+}
